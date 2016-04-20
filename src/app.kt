c82bd3c85633ce95943e13ada92c3a767297f69e
sagasu <sagasu@gmail.com>
2016-04-20 20:23:12 +0100

    playing with kotlin

--- src/app.kt
+++ src/app.kt
@@ -1,3 +1,7 @@
+data class Customer(val name: String, val email: String)
+
 fun main(args: Array<String>){
+    val customer = Customer("Jan Kowalski", "foo@bar.com")
+    println(customer)
     println("Hello world!")
 }
