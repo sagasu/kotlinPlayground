5d602e2d94e07b84280b935a2a4c55214f95ecbc
sagasu <sagasu@gmail.com>
2016-04-20 20:33:08 +0100

    playing with kotlin

--- src/app.kt
+++ src/app.kt
@@ -4,4 +4,12 @@ fun main(args: Array<String>){
     val customer = Customer("Jan Kowalski", "foo@bar.com")
     println(customer)
     println("Hello world!")
+
+    val numbers = arrayListOf(1,2,3,4,5,6)
+    println("even numbers: $(numbers.filter { it % 2 == 0 })")
+
+    val countiresCities = listOf(Pair("England", "London"), Pair("France", "Paris"), Pair("Germany", "Berlin"))
+    for((country, city) in countiresCities){
+        println("Contry: ${country} - City: ${city}")
+    }
 }
